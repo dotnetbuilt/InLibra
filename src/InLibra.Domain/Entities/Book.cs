d89b075861ac08ae5026cd2c03ed7f410c5c62df
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-12 08:30:28 +0500

    Add Book

---
+++ src/InLibra.Domain/Entities/Book.cs
@@ -0,0 +1,23 @@
+using InLibra.Domain.Commons;
+
+namespace InLibra.Domain.Entities;
+
+public class Book:BaseModel
+{
+    public string Title { get; set; }
+    public string About { get; set; }
+    public int Pages { get; set; }
+    public string Author { get; set; }
+
+    public long LanguageId { get; set; }
+    public Language Language { get; set; }
+
+    public long AttachmentId { get; set; }
+    public Attachment Attachment { get; set; }
+
+    public long UserId { get; set; }
+    public User User { get; set; }
+
+    public long ShelfId { get; set; }
+    public Shelf Shelf { get; set; }
+}

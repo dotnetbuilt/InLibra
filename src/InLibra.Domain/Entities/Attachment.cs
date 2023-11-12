053fce96bde8b131f8f03f3bf6085d48bf5a4e47
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-12 07:59:08 +0500

    Add Attachment

---
+++ src/InLibra.Domain/Entities/Attachment.cs
@@ -0,0 +1,8 @@
+namespace InLibra.Domain.Entities;
+
+public class Attachment
+{
+    public long Id { get; set; }
+    public string Filename { get; set; }
+    public string Filepath { get; set; }
+}

cbb4670b99f2601319bff4e733c241723ca1a195
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-12 07:43:28 +0500

    Add BaseModel

---
+++ src/InLibra.Domain/Commons/BaseModel.cs
@@ -0,0 +1,9 @@
+namespace InLibra.Domain.Commons;
+
+public abstract class BaseModel
+{
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}

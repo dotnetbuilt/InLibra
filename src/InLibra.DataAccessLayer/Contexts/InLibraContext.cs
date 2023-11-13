37fadc0130a5c0b4a106082cdf2dbeca153cab51
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-13 17:40:28 +0500

    Add InLibraContext

---
+++ src/InLibra.DataAccessLayer/Contexts/InLibraContext.cs
@@ -0,0 +1,18 @@
+using InLibra.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InLibra.DataAccessLayer.Contexts;
+
+public class InLibraContext:DbContext
+{
+    public InLibraContext(DbContextOptions<InLibraContext> options):base(options)
+    { }
+
+    public DbSet<Attachment> Attachments { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<BookGenre> BookGenres { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Language> Languages { get; set; }
+    public DbSet<Shelf> Shelves { get; set; }
+    public DbSet<User> Users { get; set; }
+}
--- src/InLibra.WebApi/Program.cs
+++ src/InLibra.WebApi/Program.cs
@@ -1,6 +1,17 @@
+using InLibra.DataAccessLayer.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<InLibraContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

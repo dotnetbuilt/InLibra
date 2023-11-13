677725a9efb2b091fa7cf508de694b0842a3e21a
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-13 18:17:52 +0500

    Add GenreRepository

---
+++ src/InLibra.DataAccessLayer/Repositories/GenreRepository.cs
@@ -0,0 +1,34 @@
+using InLibra.DataAccessLayer.Contexts;
+using InLibra.DataAccessLayer.Contracts;
+using InLibra.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InLibra.DataAccessLayer.Repositories;
+
+public class GenreRepository:IGenreRepository
+{
+    private readonly InLibraContext _context;
+
+    public GenreRepository(InLibraContext context)
+    {
+        _context = context;
+    }
+
+    public async ValueTask CreateAsync(Genre genre)
+    {
+        await _context.Genres.AddAsync(genre);
+        await _context.SaveChangesAsync();
+    }
+
+    public void Delete(Genre genre)
+    {
+        _context.Genres.Remove(genre);
+        _context.SaveChanges();
+    }
+
+    public async ValueTask<Genre> SelectByIdAsync(long genreId)
+        => await _context.Genres.FirstOrDefaultAsync(genre => genre.Id == genreId);
+
+    public IQueryable<Genre> SelectAsync()
+        => _context.Genres.AsQueryable();
+}

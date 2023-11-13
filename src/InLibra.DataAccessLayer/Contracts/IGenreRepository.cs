3204dc597f43635d15630149228195672bb68a79
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-13 18:12:44 +0500

    Add IGenreRepository

--- src/InLibra.DataAccessLayer/Contracts/IGenreRepository.cs
+++ src/InLibra.DataAccessLayer/Contracts/IGenreRepository.cs
@@ -1,6 +1,11 @@
+using InLibra.Domain.Entities;
+
 namespace InLibra.DataAccessLayer.Contracts;
 
 public interface IGenreRepository
 {
-    
+    ValueTask CreateAsync(Genre genre);
+    void Delete(Genre genre);
+    ValueTask<Genre> SelectByIdAsync(long genreId);
+    IQueryable<Genre> SelectAsync();
 }

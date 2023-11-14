7ff1cf13f7f1bc15b1ea257674fece347065535c
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 10:18:17 +0500

    Add CreatedAt and UpdatedAt properties to UserResultDto

--- src/InLibra.Service/DTOs/Users/UserResultDto.cs
+++ src/InLibra.Service/DTOs/Users/UserResultDto.cs
@@ -5,6 +5,8 @@ namespace InLibra.Service.DTOs.Users;
 public class UserResultDto
 {
     public long Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
     public string Firstname { get; set; }
     public string Lastname { get; set; }
     public DateTime Birthday { get; set; }

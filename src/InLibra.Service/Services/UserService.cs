2a5238e39042cd984fdcf38f705e8b0b4406e687
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 13:57:46 +0500

    Add UserService

--- src/InLibra.Service/Services/UserService.cs
+++ src/InLibra.Service/Services/UserService.cs
@@ -1,43 +1,72 @@
+using AutoMapper;
 using InLibra.DataAccessLayer.Contracts;
 using InLibra.Domain.Entities;
 using InLibra.Service.DTOs.Users;
+using InLibra.Service.Exceptions;
 using InLibra.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace InLibra.Service.Services;
 
-public class UserService:IUserService
+public class UserService : IUserService
 {
     private readonly IGenericRepository<User> _repository;
+    private readonly IMapper _mapper;
 
-    public UserService(IGenericRepository<User> repository)
+    public UserService(IGenericRepository<User> repository, IMapper mapper)
     {
         _repository = repository;
+        _mapper = mapper;
     }
 
     public async ValueTask<UserResultDto> AddAsync(UserCreationDto dto)
     {
         var user = await _repository.SelectAsync(expression: user => user.Email == dto.Email);
-        
-        if(user != 
+
+        if (user != null)
+            throw new AlreadyExistsException(message: "Email is already taken");
+
+        var newUser = _mapper.Map<User>(source: dto);
+
+        await _repository.CreateAsync(entity: newUser);
+
+        return _mapper.Map<UserResultDto>(source: newUser);
     }
 
     public async ValueTask<UserResultDto> UpdateAsync(UserUpdateDto dto)
     {
-        throw new NotImplementedException();
+        var user = await _repository.SelectAsync(expression: user => user.Id == dto.Id)
+                   ?? throw new NotFoundException(message: "User is not found");
+
+        _mapper.Map(source: dto, destination: user);
+
+        _repository.Update(entity: user);
+
+        return _mapper.Map<UserResultDto>(source: user);
     }
 
     public async ValueTask<bool> RemoveAsync(long userId)
     {
-        throw new NotImplementedException();
+        var user = await _repository.SelectAsync(expression: user => user.Id == userId)
+                   ?? throw new NotFoundException(message: "User is not found");
+
+        _repository.Delete(entity: user);
+
+        return true;
     }
 
     public async ValueTask<UserResultDto> RetrieveByIdAsync(long userId)
     {
-        throw new NotImplementedException();
+        var user = await _repository.SelectAsync(expression: user => user.Id == userId)
+                   ?? throw new NotFoundException(message: "User is not found");
+
+        return _mapper.Map<UserResultDto>(source: user);
     }
 
     public async ValueTask<IEnumerable<UserResultDto>> RetrieveAll()
     {
-        throw new NotImplementedException();
+        var users = await _repository.SelectAll().ToListAsync();
+
+        return _mapper.Map<IEnumerable<UserResultDto>>(source: users);
     }
 }

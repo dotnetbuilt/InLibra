d4ab68d01ef86968c5bf94c05d60fc96f1e4e556
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 12:50:38 +0500

    Add IUserService

---
+++ src/InLibra.Service/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+using InLibra.DataAccessLayer.Contracts;
+using InLibra.Domain.Entities;
+using InLibra.Service.DTOs.Users;
+
+namespace InLibra.Service.Interfaces;
+
+public interface IUserService
+{
+    ValueTask<UserResultDto> AddAsync(UserCreationDto dto);
+    ValueTask<UserResultDto> UpdateAsync(UserUpdateDto dto);
+    ValueTask<bool> RemoveAsync(long userId);
+    ValueTask<UserResultDto> RetrieveByIdAsync(long userId);
+    ValueTask<IEnumerable<UserResultDto>> RetrieveAll();
+}

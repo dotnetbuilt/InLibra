50725127f62f9657509f4d1d3b059f1efe85d87b
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 10:22:55 +0500

    Add BookCreationDto

---
+++ src/InLibra.Service/DTOs/Books/BookCreationDto.cs
@@ -0,0 +1,13 @@
+namespace InLibra.Service.DTOs.Books;
+
+public class BookCreationDto
+{
+    public string Title { get; set; }
+    public string About { get; set; }
+    public int Pages { get; set; }
+    public string Author { get; set; }
+    public long LanguageId { get; set; }
+    public long AttachmentId { get; set; }
+    public long UserId { get; set; }
+    public long ShelfId { get; set; }
+}

cf3eca77976e79f70a761c9eda84db625ac68c1d
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 13:35:29 +0500

    Add CustomException

---
+++ src/InLibra.Service/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+namespace InLibra.Service.Exceptions;
+
+public class CustomException:Exception
+{
+    public int StatusCode { get; set; }
+
+    public CustomException(string message, int statusCode):base(message)
+    {
+        StatusCode = statusCode;
+    }
+}

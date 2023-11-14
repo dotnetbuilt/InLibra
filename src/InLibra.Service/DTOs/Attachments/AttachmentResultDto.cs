934d71ae14dcd4da4f0f2fb547acb37b8e263a6a
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 10:38:59 +0500

    Add AttachmentResultDto

---
+++ src/InLibra.Service/DTOs/Attachments/AttachmentResultDto.cs
@@ -0,0 +1,8 @@
+namespace InLibra.Service.DTOs.Attachments;
+
+public class AttachmentResultDto
+{
+    public long Id { get; set; }
+    public string Filename { get; set; }
+    public string Filepath { get; set; }
+}
---
+++ src/InLibra.Service/Mappers/MappingProfile.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace InLibra.Service.Mappers;
+
+public class MappingProfile:Profile
+{
+    public MappingProfile()
+    {
+        //
+        CreateMap<>()
+    }
+}

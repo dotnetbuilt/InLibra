46e6069ab19afaa802a8a4f45513130005c45c44
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 09:47:22 +0500

    Add LanguageResultDto

---
+++ src/InLibra.Service/DTOs/Languages/LanguageResultDto.cs
@@ -0,0 +1,8 @@
+namespace InLibra.Service.DTOs.Languages;
+
+public class LanguageResultDto
+{
+    public long Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+}

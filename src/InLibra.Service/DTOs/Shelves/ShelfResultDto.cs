0977aeab660eeaee1d8d842b977e1d01e6612a77
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 10:01:36 +0500

    Add ShelfResultDto

---
+++ src/InLibra.Service/DTOs/Shelves/ShelfResultDto.cs
@@ -0,0 +1,9 @@
+namespace InLibra.Service.DTOs.Shelves;
+
+public class ShelfResultDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}

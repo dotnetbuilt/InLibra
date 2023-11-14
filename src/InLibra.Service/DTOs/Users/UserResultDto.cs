0f3ba0015dfeffdacb9e459328ae613e1e2969bf
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 10:14:26 +0500

    Add UserResultDto

---
+++ src/InLibra.Service/DTOs/Users/UserResultDto.cs
@@ -0,0 +1,13 @@
+using InLibra.Domain.Enums;
+
+namespace InLibra.Service.DTOs.Users;
+
+public class UserResultDto
+{
+    public long Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public DateTime Birthday { get; set; }
+    public Gender Gender { get; set; }
+    public string Email { get; set; }
+}

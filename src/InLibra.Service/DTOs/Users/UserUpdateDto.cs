28cd69be2ce55e36aa41da9a656cb8934de6784b
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-14 10:09:23 +0500

    Add UserUpdateDto

---
+++ src/InLibra.Service/DTOs/Users/UserUpdateDto.cs
@@ -0,0 +1,8 @@
+namespace InLibra.Service.DTOs.Users;
+
+public class UserUpdateDto
+{
+    public long Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+}

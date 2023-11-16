63329c537e4a6de1a6fb0d7eff0d16d473921c0f
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-16 18:26:46 +0500

    Add IBookShelfService

---
+++ src/InLibra.Service/Interfaces/IBookShelfService.cs
@@ -0,0 +1,11 @@
+using InLibra.Domain.Entities;
+using InLibra.Service.DTOs.BookShelves;
+
+namespace InLibra.Service.Interfaces;
+
+public interface IBookShelfService
+{
+    ValueTask<BookShelfResultDto> AddAsync(BookShelfCreationDto dto);
+    ValueTask<bool> RemoveAsync(long bookShelfId);
+    ValueTask<BookShelfResultDto> RetrieveByIdAsync(long bookShelfId);
+}

d41e784e8927f4e49495bc097e54fc949e456242
abbosZoirov1 <122oszoirov@gmail.com>
2023-11-17 15:27:36 +0500

    Add UsersController

--- src/InLibra.WebApi/Controllers/UsersController.cs
+++ src/InLibra.WebApi/Controllers/UsersController.cs
@@ -1,10 +1,11 @@
 using InLibra.Service.DTOs.Users;
 using InLibra.Service.Interfaces;
+using InLibra.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InLibra.WebApi.Controllers;
 
-public class UsersController:BaseController
+public class UsersController : BaseController
 {
     private readonly IUserService _service;
 
@@ -12,6 +13,49 @@ public class UsersController:BaseController
     {
         _service = service;
     }
-    
-    
+
+    [HttpPost("register")]
+    public async ValueTask<IActionResult> RegisterAsync(UserCreationDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _service.AddAsync(dto)
+        });
+
+    [HttpPut("update")]
+    public async ValueTask<IActionResult> UpdateAsync(UserUpdateDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _service.UpdateAsync(dto)
+        });
+
+    [HttpDelete("delete")]
+    public async ValueTask<IActionResult> DeleteAsync(long userId)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _service.RemoveAsync(userId)
+        });
+
+    [HttpGet("get-by-id")]
+    public async ValueTask<IActionResult> GetByIdAsync(long userId)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _service.RetrieveByIdAsync(userId)
+        });
+
+    [HttpGet("get-all")]
+    public async ValueTask<IActionResult> GetAllAsync()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _service.RetrieveAllAsync()
+        });
 }
